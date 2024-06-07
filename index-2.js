d1e69f46dd3f3cce7fe44598bd2678c115ac38a6
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-06-07 20:28:01 +0300

    .

---
+++ index-2.js
@@ -0,0 +1,60 @@
+// const tech = [
+//     { label: 'HTML' },
+//     { label: 'CSS' },
+//     { label: 'JavaScript' },
+//     { label: 'Node.js' },
+//     { label: 'React' },
+//     { label: 'Vue' },
+//     { label: 'Next.js' },
+//     { label: 'Mobx' },
+//     { label: 'Redux' },
+//     { label: 'React Router' },
+//     { label: 'GraphQl' },
+//     { label: 'PostgreSQL' },
+//     { label: 'MongoDB' },
+//     ];
+
+
+//     const techList = document.querySelector('.js-list')
+//     function createListEl(techSkills){
+// return techSkills.map((skill)=>{
+// return `<li>${skill.label}</li>`
+// }).join(' ')
+
+//     }
+
+//    const listMarkup = createListEl(tech)
+//    techList.insertAdjacentHTML('beforeend', listMarkup)
+
+
+
+
+//    const people = [
+//     { name: "John", age: 25 },
+//     { name: "Jane", age: 30 },
+//     { name: "Mike", age: 20 },
+//     { name: "Alice", age: 28 },
+//     { name: "Bob", age: 35 },
+//     { name: "Emily", age: 22 },
+//     { name: "Chris", age: 27 },
+//     { name: "Anna", age: 32 },
+//     { name: "James", age: 24 },
+//     { name: "Sophia", age: 29 }
+//     ];
+
+
+
+
+//     const peopleList = document.querySelector('.js-list')
+//     function chekPeopleEl(chekList){
+//         return chekList.map((people)=>{
+// return ` <li>
+// <p>Імя:${people.name} </p>
+// <p>Вік:${people.age} </p>
+// </li>`
+
+//         }).join(' ')
+//     }
+
+// const peopleMarkup = chekPeopleEl(people)
+//     peopleList.insertAdjacentHTML('beforeend', peopleMarkup)
--- index.js
+++ index.js
@@ -70,37 +70,37 @@
 * Mousemove и throttle
 */
 
-const coordsOutputRef = document.querySelector('.js-coords');
-let mouseMoveCbInvocationCounter = 0;
+// const coordsOutputRef = document.querySelector('.js-coords');
+// let mouseMoveCbInvocationCounter = 0;
 
-window.addEventListener('mousemove', _.throttle(onMouseMove, 500));
+// window.addEventListener('mousemove', _.throttle(onMouseMove, 500));
 
-function onMouseMove(event) {
-mouseMoveCbInvocationCounter += 1;
+// function onMouseMove(event) {
+// mouseMoveCbInvocationCounter += 1;
 
-coordsOutputRef.textContent = `
-Кількість викликів onMouseMove: ${mouseMoveCbInvocationCounter},
-X: ${event.clientX},
-Y:${event.clientY}
-`;
-}
+// coordsOutputRef.textContent = `
+// Кількість викликів onMouseMove: ${mouseMoveCbInvocationCounter},
+// X: ${event.clientX},
+// Y:${event.clientY}
+// `;
+// }
 
 /*
 * Input и debounce
 */
-const inputRef = document.querySelector('.js-input');
-const outputRef = document.querySelector('.js-output');
-let inputCbInvocationCounter = 0;
-const option = { trailing: true, leading: true }
+// const inputRef = document.querySelector('.js-input');
+// const outputRef = document.querySelector('.js-output');
+// let inputCbInvocationCounter = 0;
+// const option = { trailing: true, leading: true }
 
 
-inputRef.addEventListener('input', _.debounce(onInputChange, 500));
+// inputRef.addEventListener('input', _.debounce(onInputChange, 500));
 
-function onInputChange(event) {
-inputCbInvocationCounter += 1;
+// function onInputChange(event) {
+// inputCbInvocationCounter += 1;
 
-outputRef.textContent = `
-Кількість викликів onInputChange: ${inputCbInvocationCounter},
-Значення: ${event.target.value}
-`;
-}
+// outputRef.textContent = `
+// Кількість викликів onInputChange: ${inputCbInvocationCounter},
+// Значення: ${event.target.value}
+// `;
+// }
